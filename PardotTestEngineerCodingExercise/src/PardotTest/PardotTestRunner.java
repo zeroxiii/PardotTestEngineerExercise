1a33f1d8e40510305e3fb9e1f058cde8158dcbf1
zeroxiii <rahmaan.lodhia@gmail.com>
2016-02-23 10:45:18 -0500

    Updated comments

--- PardotTestEngineerCodingExercise/src/PardotTest/PardotTestRunner.java
+++ PardotTestEngineerCodingExercise/src/PardotTest/PardotTestRunner.java
@@ -8,13 +8,17 @@ public class PardotTestRunner
 {
    public static void main(String[] args) 
    {
+	  // Run all the test cases in PardotTest
       Result result = JUnitCore.runClasses(PardotTest.class);
       
+      // Check to see if any failures occurred
       for (Failure failure : result.getFailures()) 
       {
          System.out.println(failure.toString());
       }
       
-      System.out.println(result.wasSuccessful());
+      // If no failures present, test was successful
+      if (result.wasSuccessful())
+    	  System.out.println("All test steps completed successfully!");
    }
 } 
